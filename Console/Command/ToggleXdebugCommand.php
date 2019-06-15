ccfedcc204b1944a4034686ab9d28e5704530fd7
Douglas Munro <douglas.munro@epicor.com>
2019-06-15 17:47:44 +0100

    initial commit

---
+++ Console/Command/ToggleXdebugCommand.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright © 2010-2018 Epicor Software Corporation: All Rights Reserved
+ */
+
+namespace Zeloc\XdebugToggle\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ToggleXdebugCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('zeloc:xdebug:toggle');
+        $this->setDescription('Toggles xdebug on or off');
+
+        parent::configure();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $xdebugIni = file_get_contents('/etc/php/7.1/mods-available/xdebug.ini');
+        preg_match('/^;z/', $xdebugIni, $matches);
+        $result = $matches[0] ?? false;
+
+        if ($result) {
+            $out = preg_replace('/^;z/', 'z', $xdebugIni);
+            $output->writeln('Setting Xdebug ON');
+        } else {
+            $out = preg_replace('/^z/', ';z', $xdebugIni);
+            $output->writeln('Setting Xdebug OFF');
+        }
+        file_put_contents('/etc/php/7.1/mods-available/xdebug.ini', $out);
+        shell_exec('sudo service php7.1-fpm restart');
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+
+This version is for php 7.1
+```
+composer require zeloc/xdebugtoggle
+```
+The above represents the name that is defined in the modules composer.json
+
+Runs with n98 mage run or bin/magento
+```
+zeloc:xdebug:toggle  or short z:x:t
+
+```
+
+```
