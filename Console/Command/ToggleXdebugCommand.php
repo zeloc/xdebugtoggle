82c2ed5bdcbc4b0e925ecafb99cad606827d5a77
Douglas Munro <munrod@tcd.ie>
2019-06-23 16:03:07 +0100

    update style output with more feedback
    (cherry picked from commit 8decf79bfbefd8435ebc84a9e8678e92c1e1fa30)

--- Console/Command/ToggleXdebugCommand.php
+++ Console/Command/ToggleXdebugCommand.php
@@ -18,18 +18,32 @@ class ToggleXdebugCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $xdebugIniFile = '/etc/php/7.1/mods-available/xdebug.ini';
+        if(!is_writable($xdebugIniFile)){
+            $output->writeln('<fg=red>Can\'t update xdebug.ini >>> Update permissions to make xdebug.ini writeable</>');
+        }
+        $output->writeln('');
+        $output->writeln('<question>########    Toggle Xdebug on/off    #########</question>');
+        $output->writeln('');
         $xdebugIni = file_get_contents('/etc/php/7.1/mods-available/xdebug.ini');
         preg_match('/^;z/', $xdebugIni, $matches);
         $result = $matches[0] ?? false;
 
         if ($result) {
             $out = preg_replace('/^;z/', 'z', $xdebugIni);
-            $output->writeln('Setting Xdebug ON');
+            $resultOutput = '<info>Xdebug Status: </info><fg=blue>ON</>';
         } else {
             $out = preg_replace('/^z/', ';z', $xdebugIni);
-            $output->writeln('Setting Xdebug OFF');
+            $resultOutput = '<info>Xdebug Status: </info><fg=blue>OFF</>';
         }
-        file_put_contents('/etc/php/7.1/mods-available/xdebug.ini', $out);
+        $output->writeln('Updating xdebug.ini file....');
+        file_put_contents($xdebugIniFile, $out);
+        $output->writeln('Restarting php fpm service....');
         shell_exec('sudo service php7.1-fpm restart');
+
+        $output->writeln($resultOutput);
+        $output->writeln('');
+        $output->writeln('<question>#############################################</question>');
+        $output->writeln('');
     }
 }
